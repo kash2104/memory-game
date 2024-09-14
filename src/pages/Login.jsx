2ed1adf2ae80a58488db584a8fd81d40f04b666f
kash2104 <kparikh1104@gmail.com>
2024-09-14 09:56:39 +0530

    login and signup

---
+++ src/pages/Login.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import Template from "../components/auth/Template";
+
+function Login() {
+  return <Template text="Welcome again" formType="login" />;
+}
+
+export default Login;
---
+++ src/pages/Signup.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import Template from "../components/auth/Template";
+
+function Signup() {
+  return <Template text="Welcome" formType="signup" />;
+}
+
+export default Signup;
